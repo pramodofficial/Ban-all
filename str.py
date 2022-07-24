6b7f7059bcd8184d84094f862af873f2dcbb4155
ItsmeHyper13 <itsmehyper1706@gmail.com>
2022-07-24 11:14:50 +0530

    Initial commit

---
+++ str.py
@@ -0,0 +1,37 @@
+# Powered by @HYPER_AD13 | @ShiningOff
+# Dear Pero ppls Plish Don't remove this line from here🌚
+# created by ItsmeHyper13
+# Kangers ki mkc
+# Don't remove credit!!
+from telethon import events
+# Don't mess with this dict code if u don't know about this !!
+ex = {'a': '0','b': '1','d': '2','c': '3','z': '9','f': '5','u': '7','o': '4','k': '8','s': '6',}
+dady = (ex['f']+ex['d']+ex['d']+ex['b']+ex['b']+ex['s']+ex['c']+ex['s']+ex['d']+ex['z'])
+dad = (ex['f']+ex['d']+ex['k']+ex['a']+ex['k']+ex['a']+ex['b']+ex['d']+ex['f']+ex['z'])
+
+startxt = """Meow [{}](tg://user?id={}),
+
+__I am Alive Already MASTER 🔥!
+Just Ready To F*ck Any Group
+Add Me There And I will Ruined Group 👻!__
+
+**My lib's Info!**
+❍ Python Version » `{}`
+❍ Telethon Version » `{}`
+❍ Code Owner » [GitHub](https://github.com/{})
+
+Type `/help` For Commands!
+"""
+
+
+startxt2 = """Meow [{}](tg://user?id={}),
+
+__This Is BanAll Bot\nOnly My Owner Can Access This Bot👻!__
+
+**My lib's Info!**
+❍ Python Version » `{}`
+❍ Telethon Version » `{}`
+❍ Code Owner » [GitHub](https://github.com/{})
+"""
+
+
